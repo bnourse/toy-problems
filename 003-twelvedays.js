b95655df2bd3ec3f7cd8c711d99c4204754596c3
Ben Nourse <bnourse@gmail.com>
2017-01-25 10:05:25 -0600

    add conditional and

--- 003-twelvedays.js
+++ 003-twelvedays.js
@@ -1,9 +1,13 @@
 function buildDay(dayCount) {
+
 	var dayWords = ["zeroth", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
-	var dayText = ["", "a Partridge in a Pear Tree", "Two Turtle Doves", "Three French Hens", "Four Calling Birds", "5 Golden Rings", "6 Geese a Laying","7 Swans a Swimming","8 Maids a Milking","9 Ladies Dancing", "10 Lords a Leaping","11 Pipers Piping","12 Drummers Drumming"];
+	var dayText = ["", "a Partridge in a Pear Tree", "Two Turtle Doves", "Three French Hens", "Four Calling Birds", "five Golden Rings", "six Geese a Laying","seven Swans a Swimming","eight Maids a Milking","nine Ladies Dancing", "ten Lords a Leaping","elevn Pipers Piping","twelve Drummers Drumming"];
 	console.log("On the " + dayWords[dayCount] + " day of Christmas\nmy true love sent to me: ")
-	for(var day=dayCount; dayCount >= 1; dayCount--) {
-		console.log(dayText[dayCount]);
+	for(var day=dayCount; day >= 1; day--) {
+		if(dayCount > 1 && day == 1) {
+			console.log("and " + dayText[day]);
+		}
+		else console.log(dayText[day]);
 	}
 
 }
